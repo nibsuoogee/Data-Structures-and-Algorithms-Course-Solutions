c3f047f0f48faff8ec1010f86334edc9d3ce48cd
nibsuoogee <elias.syyrila@gmail.com>
2022-11-29 17:03:06 +0200

    initial task version

---
+++ binpack.py
@@ -0,0 +1,23 @@
+def binpack(items, S):
+    
+    return
+    
+ 
+if __name__ == "__main__":
+
+    items = [9, 3, 3, 6, 10, 4, 6, 8, 6, 3]
+    B = 10
+
+    bins = binpack(items, B)
+
+    for i in range(len(bins)):
+        print(f"bin {i+1}: {bins[i]}")
+
+# A possible output:
+#   bin 1: [9]
+#   bin 2: [3, 3, 4]
+#   bin 3: [6, 3]
+#   bin 4: [10]
+#   bin 5: [6]
+#   bin 6: [8]
+#   bin 7: [6]
